7858b326da4066a702848c03cc61d83efb0bdff1
Inna Vynogradova <inna.vinogradova80@gmail.com>
2022-06-07 22:15:46 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML('beforeend', createImageGalleryMarkup(galleryItems));
+
+function createImageGalleryMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+        })
+        .join("");
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250ms',
+});
